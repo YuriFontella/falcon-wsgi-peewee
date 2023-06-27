2db1819cf5234b194dcfc654a95cbd2623ad9354
Yuri <yurifc4@gmail.com>
2023-06-27 17:48:15 -0300

    migrations

--- config/migrations/main.py
+++ config/migrations/main.py
@@ -4,11 +4,6 @@ from src.models.users import Users
 
 from config.db.pool import db
 
-try:
-    Users.create_table()
-except peewee.OperationalError as e:
-  	print(e)
-
 def create_tables():
    with db:
         db.create_tables([Users])
