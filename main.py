43b10773285d77f6c65ce70138854f3acd51a21a
Yuri <yurifc4@gmail.com>
2023-06-28 10:06:12 -0300

    middleware

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from errors.storage import StorageError
 
 from routes import users, suffix
 
-app = falcon.App(middleware=[ PoolMiddleware(), AuthMiddleware()])
+app = falcon.App(middleware=[PoolMiddleware(), AuthMiddleware()])
 
 users.routes(app)
 suffix.routes(app)
